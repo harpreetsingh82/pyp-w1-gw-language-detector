af9c0014850f62f106b730618f482602a6eeb641
Harpreet <harpreet.bharaj@gmail.com>
2016-07-08 03:32:25 +0000

    Project 1Group 8

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -6,4 +6,26 @@ LANGUAGES = [
         'common_words': ['yo', 'tu', 'el', 'nosotros']  # add your words here
     },
     #  include your extra languages here
+    
+    
+    {
+        'name': 'German',
+        'common_words' : [
+                    'das', 'ist', 'du', 'ich', 'nicht', 'die', 'es', 'und',
+                    'der', 'was', 'wir', 'zu', 'ein', 'er', 'in', 'sie', 'mir',
+                    'mit', 'ja', 'wie', 'den', 'auf', 'mich', 'dass', 'so',
+                    'hier', 'eine', 'wenn', 'hat', 'all', 'sind', 'von',
+                    'dich', 'war', 'haben', 'für', 'an', 'habe', 'da', 'nein',
+                    'bin', 'noch', 'dir', 'uns', 'sich', 'nur',
+                    'einen', 'kann', 'dem'
+                ]
+    },
+    
+    { 
+	'name' : 'English',
+	'common_words' : ['we', 'the', 'them', 'there', 'us', 'hello', 'name', 'my',
+	'what', 'why', 'where', 'get', 'her', 'she']
+    }
+    
 ]
+  
--- language_detector/main.py
+++ language_detector/main.py
@@ -6,4 +6,12 @@ from .languages import LANGUAGES
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    text = text.lower()
+    count = {}
+    for i, val in enumerate(LANGUAGES):
+        ct=0
+        for word in text.split(' '):
+            if word in LANGUAGES[i]['common_words']:
+                ct +=1
+        count[LANGUAGES[i]['name']]= ct
+    return max(count, key=count.get)
